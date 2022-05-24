0235d906496e37508c89436217ca1459dc27f5ea
sarva-az <94432806+sarva-az@users.noreply.github.com>
2022-05-24 18:36:38 +0530

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,9 +14,16 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder1(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        public static IHostBuilder CreateHostBuilder1(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
